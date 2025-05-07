155ded6c7eb998fccc589158dce1c4a61f946022
Imaaaann <158160599+Imaaaann@users.noreply.github.com>
2025-05-07 17:17:30 +0000

    Added basic javascript and typewriter plugin

--- index.js/recipe.js
+++ index.js/recipe.js
@@ -1,1 +1,12 @@
-alert("hello")
+function generateRecipe(event) {
+    event.preventDefault()
+    new Typewriter('#recipe', {
+        strings: "Generating recipe",
+        autoStart: true,
+        cursor: "",
+        delay:"50",
+      });
+}
+
+let ingFormElement = document.querySelector("#ing-form")
+ingFormElement.addEventListener("submit",generateRecipe)
